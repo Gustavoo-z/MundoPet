75c7e8838ad7ba300a29878229fb7b2bc9161953
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-01-23 10:26:13 -0300

    Tela upload finalizada

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const botaoUpload = document.getElementById('upload-btn');
+const inputUpload = document.getElementById('image-upload');
+
+botaoUpload.addEventListener('click', () => {
+    inputUpload.click();
+})

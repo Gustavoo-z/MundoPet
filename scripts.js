701a085a9ffd41941006ad5555c742b3134f0739
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-01-25 15:40:16 -0300

    Adição de Hashtags

--- scripts.js
+++ scripts.js
@@ -42,3 +42,38 @@ inputUpload.addEventListener('change', async (evento) => {
         }
     } 
 })
+
+const inputHashtag = document.getElementById('categoria');
+const listaTags = document.getElementById('lista-tags');
+
+inputHashtag.addEventListener("keypress", (evento) => {
+    if (evento.key === "Enter") {
+    evento.preventDefault();
+    const valorHashtag = inputHashtag.value.trim();
+        if (valorHashtag !== "" && tagsPermitidas.includes(valorHashtag)) {
+        const novaHashtag = document.createElement("li");
+        novaHashtag.innerHTML = `<p>${valorHashtag}</p> <img src="/img/close-black.png" class="remove-tag">`;
+        listaTags.appendChild(novaHashtag);
+        inputHashtag.value = "";
+        } else {
+            alert("Tag inválida!");
+        }
+    }
+})
+
+listaTags.addEventListener('click', (evento) => {
+    if (evento.target.classList.contains("remove-tag")) {
+        const hashtagRemover = evento.target.parentElement;
+        listaTags.removeChild(hashtagRemover);
+    }
+})
+
+const tagsPermitidas = ["Gato", "Cachorro", "Bichinho", "Fofo"];
+
+async function verificarTagsMundoPet(valorHashtag) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(tagsPermitidas.contains(valorHashtag));
+        }, 1000)
+    })
+}
